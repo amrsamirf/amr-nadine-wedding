720175b6ad665c452e77d514440cbd575f1a7e4c
Amr Samir <a.samir@penta-b.com>
2026-03-11 20:04:47 +0200

    build arabic version

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath, URL } from "node:url";
+import { resolve } from "node:path";
 
 export default defineConfig({
   base: "/amr-nadine-wedding/",
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        ar: resolve(__dirname, "ar/index.html"),
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
